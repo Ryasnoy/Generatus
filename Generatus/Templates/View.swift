17bf975aef6d8a7b3743c91ca1c511b3ee55a046
Oleg <11443671+Ryasnoy@users.noreply.github.com>
2021-03-13 22:02:35 +0200

    SwiftUI + MVVM templates

---
+++ Generatus/Templates/View.swift
@@ -0,0 +1,19 @@
+//
+//  ⚡️Created by Generatus⚡️ on $CREATE_DATE
+//  https://github.com/Ryasnoy/Generatus
+//
+//  $MODULE_NAMEView.swift
+//
+//  Created by $USER
+//  Copyright © $USER. All rights reserved.
+//
+
+import SwiftUI
+
+struct $MODULE_NAMEView: View {
+    
+    var body: some View {
+        Text("$MODULE_NAME")
+    }
+    
+}
---
+++ Generatus/Templates/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ⚡️Created by Generatus⚡️ on $CREATE_DATE
+//  https://github.com/Ryasnoy/Generatus
+//
+//  $MODULE_NAMEViewModel.swift
+//
+//  Created by $USER
+//  Copyright © $USER. All rights reserved.
+//
+import Foundation
+
+final class $MODULE_NAMEViewModel: ObservableObject {
+    
+}
